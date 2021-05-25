4d8d6b16336104d69f94a875b2807ae6967135bd
Pushkar Sharma <pushkar1291@gmail.com>
2021-05-25 19:08:47 +0530

    underscores in literals

---
+++ java7/underscores-in-numerals/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+  public static void main(String[] args) {
+    int i = 1_00_00_000;
+    System.out.println("Integer: " + i);
+    long l = 10_000_000_456_789L;
+    System.out.println("Long: " + l);
+    float f = 3.14_13_56F;
+    System.out.println("Float: " + f);
+    double d = 64_345_453.343_423_543;
+    System.out.println("Double: " + d);
+    long lb = 0b100_000_111_010_110;
+    System.out.println("Decimal of Binary representation: " + lb);
+  }
+}
